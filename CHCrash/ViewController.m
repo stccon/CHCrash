914ec19c39e63e9d200e1ef94eafc1837aa02003
stccon <1013124327@qq.com>
2016-11-21 17:19:25 +0800

    防止nsmutablearr 越界

--- CHCrash/ViewController.m
+++ CHCrash/ViewController.m
@@ -17,16 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //截获label的text被赋值为空
+    //label的text被赋值为空
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
     [self.view addSubview:label];
     label.text = nil;
 
-    //截获数组越界
+    //数组越界
     NSArray *arr = @[@"aa",@"bb"];
     NSString *str = [arr objectAtIndex:10];
     NSLog(@"%@",str);
-}
+    
+    //数组越界
+    NSMutableArray *mArr = [NSMutableArray arrayWithArray:@[@"aa",@"bb"]];
+    NSString *str1 = [mArr objectAtIndex:10];
+    NSLog(@"%@",str1);}
 
 
 - (void)didReceiveMemoryWarning {
